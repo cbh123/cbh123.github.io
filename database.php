a6bdaacd093b1580cc32737ca8f56524b8750eb8
cbh123 <charlieholtz@gmail.com>
2016-11-28 14:10:50 +0900

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,7 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your email address is: <?php echo $_POST["theDocketInput"]; ?>
 
 </body>
 </html>
